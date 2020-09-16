5e4b9737f6d66e8a39c13291c0238ebfd40db19c
zt <ztcjoe93@gmail.com>
2020-09-16 14:55:23 +0800

    CardPerformance chart and datatable implementation
    Signed-off-by: zt <ztcjoe93@gmail.com>

--- lib/Charts/CardPerformance.dart
+++ lib/Charts/CardPerformance.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:memory_cards/Objects/objects.dart';
 
-class CardPerformance extends StatelessWidget {
+class CardPerformance extends StatefulWidget {
   final List<Result> results;
   String data;
-  var listResults;
 
   CardPerformance(this.results, this.data);
 
-  // consolidate all card results
+  @override
+  _CardPerformanceState createState() => _CardPerformanceState();
+}
+
+class _CardPerformanceState extends State<CardPerformance> {
+  List<dynamic> consolidatedList = [];
+  bool _sorted = false;
+  int _colInd = 0;
+  var listResults;
+
   consolidate(BuildContext context){
     final consolidatedResults = Map();
     // iterate over each card in result
-    for (var result in results){
+    for (var result in widget.results){
       for (var card in result.results) {
         // add to map if not exists, +1 to count, +1 to score if true
         if (!consolidatedResults.containsKey(card.card.front)){
@@ -34,40 +42,99 @@ class CardPerformance extends StatelessWidget {
       consolidatedResults[key]['%'] = 100 * (value['score']/value['count']);
     });
 
-    var consolidatedList = [];
 
-    if (data == "chart"){
+    if (widget.data == "chart"){
       return generateChart(consolidatedResults);
     } else {
-      consolidatedResults.forEach((key, value){
-        consolidatedList.add({
-          'front': key,
-          'back': value['back'],
-          '%': value['%'],
-          'score': value['score'],
-          'tries': value['count'],
-        });
-      });
-      
-      return generateTable(consolidatedList, context);
+      if (consolidatedList.length == 0) {
+        consolidatedList = consolidatedResults.entries.map((r) =>
+        {
+          'front': r.key,
+          'back': r.value['back'],
+          '%': r.value['%'],
+          'score': r.value['score'],
+          'tries': r.value['count'],
+        }
+        ).toList();
+      }
+
+      return generateTable(context);
     }
   }
 
-  generateTable(data, context){
+  sortFunction(int columnIndex, bool asc, String key){
+    setState(() {
+      _sorted = !_sorted;
+      _colInd = columnIndex;
+      if (_sorted) {
+        consolidatedList.sort((a, b) => a[key].compareTo(b[key]));
+      } else {
+        consolidatedList.sort((a, b) => b[key].compareTo(a[key]));
+      }
+    });
+  }
+
+  generateTable(context){
     return SingleChildScrollView(
       scrollDirection: Axis.vertical,
       child: DataTable(
+        sortAscending: _sorted,
+        sortColumnIndex: _colInd,
         columnSpacing: MediaQuery.of(context).size.width * 0.085,
         columns: [
-          DataColumn(label: Text('Front')),
-          DataColumn(label: Text('Back')),
-          DataColumn(label: Text('%')),
-          DataColumn(label: Text('Score')),
-          DataColumn(label: Text('Tries')),
+          DataColumn(
+            onSort: (columnIndex, ascending) =>
+                sortFunction(columnIndex, ascending, "front"),
+            label: Text('Front'),
+          ),
+          DataColumn(
+            onSort: (columnIndex, ascending) =>
+                sortFunction(columnIndex, ascending, "back"),
+            label: Text('Back'),
+          ),
+          DataColumn(
+            onSort: (columnIndex, ascending) =>
+                sortFunction(columnIndex, ascending, "%"),
+            label: Text('%'),
+          ),
+          DataColumn(
+            onSort: (columnIndex, ascending) =>
+                sortFunction(columnIndex, ascending, "score"),
+            label: Text('Score'),
+          ),
+          DataColumn(
+            onSort: (columnIndex, ascending) =>
+                sortFunction(columnIndex, ascending, "tries"),
+            label: Text('Tries'),
+          ),
         ],
-        rows: [
-          for(var datum in data)
-          DataRow(
+        rows: consolidatedList.map((res) =>
+            DataRow(
+              cells: [
+                DataCell(Container(
+                  child: Text("${res['front']}"),
+                  width: MediaQuery.of(context).size.width * 0.1,
+                )),
+                DataCell(Container(
+                  child: Text("${res['back']}"),
+                  width: MediaQuery.of(context).size.width * 0.1,
+                )),
+                DataCell(Container(
+                  child: Text("${res['%'].toStringAsFixed(2)}"),
+                  width: MediaQuery.of(context).size.width * 0.1,
+                )),
+                DataCell(Container(
+                  child: Text("${res['score']}"),
+                  width: MediaQuery.of(context).size.width * 0.1,
+                )),
+                DataCell(Container(
+                  child: Text("${res['tries']}"),
+                  width: MediaQuery.of(context).size.width * 0.1,
+                )),
+              ]
+            )
+        ).toList(),
+            /*
             color: MaterialStateProperty.resolveWith((states){
               if (datum['%'] >= 75.0){
                return Colors.lightGreen.withOpacity(0.5);
@@ -77,40 +144,18 @@ class CardPerformance extends StatelessWidget {
                 return Colors.yellow.withOpacity(0.5);
               }
             }),
-            cells: [
-              DataCell(Container(
-                child: Text("${datum['front']}"),
-                width: MediaQuery.of(context).size.width * 0.1,
-              )),
-              DataCell(Container(
-                child: Text("${datum['back']}"),
-                width: MediaQuery.of(context).size.width * 0.1,
-              )),
-              DataCell(Container(
-                child: Text("${datum['%'].toStringAsFixed(2)}"),
-                width: MediaQuery.of(context).size.width * 0.1,
-              )),
-              DataCell(Container(
-                child: Text("${datum['score']}"),
-                width: MediaQuery.of(context).size.width * 0.1,
-              )),
-              DataCell(Container(
-                child: Text("${datum['tries']}"),
-                width: MediaQuery.of(context).size.width * 0.1,
-              )),
-            ],
-          ),
-        ],
+             */
       ),
     );
   }
 
   generateChart(data){
-    final chartData = [
-      {'domain': 0, 'count': 0},
-      {'domain': 1, 'count': 0},
-      {'domain': 2, 'count': 0},
+    final List<Map<String, dynamic>> chartData = [
+      {'domain': "Great", 'count': 0},
+      {'domain': "Good", 'count': 0},
+      {'domain': "Poor", 'count': 0},
     ];
+    int totalCount = 0;
 
     for (var key in data.keys){
       if(data[key]['%'] >= 75.0) {
@@ -120,20 +165,27 @@ class CardPerformance extends StatelessWidget {
       } else {
         chartData[1]['count']++;
       }
+      totalCount++;
     }
 
     chartData.removeWhere((element) => element['count'] == 0);
 
     return [
-      charts.Series<dynamic, int>(
+      charts.Series<dynamic, dynamic>(
         id: 'Card Performance',
+        colorFn: (dynamic datum, _) => charts.ColorUtil.fromDartColor(
+          datum['domain'] == "Great"
+              ? Colors.lightGreen.withOpacity(0.5)
+              : datum['domain'] == "Good"
+                ? Colors.yellow.withOpacity(0.5)
+                : Colors.redAccent.withOpacity(0.5)
+        ),
         domainFn: (dynamic datum, _) => datum['domain'],
         measureFn: (dynamic datum, _) => datum['count'],
-        labelAccessorFn: (dynamic datum, _){
-          return datum['domain'] == 0
-              ? "Great"
-              : datum['domain'] == 1 ? "Good" : "Poor";
-        },
+        // https://google.github.io/charts/flutter/example/pie_charts/auto_label
+        labelAccessorFn: (dynamic datum, _) =>
+          // convert to 2 d.p string
+          "${(100 * datum['count']/totalCount).toStringAsFixed(2)}%",
         data: chartData,
       )
     ];
@@ -141,15 +193,42 @@ class CardPerformance extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (data == "chart") {
+    if (widget.data == "chart") {
       return charts.PieChart(
         consolidate(context),
         animate: false,
+        // https://google.github.io/charts/flutter/example/legends/datum_legend_options
+        behaviors: [
+          charts.DatumLegend(
+            position: charts.BehaviorPosition.end,
+            insideJustification: charts.InsideJustification.topEnd,
+            horizontalFirst: false,
+            desiredMaxRows: 3,
+            cellPadding: EdgeInsets.all(4.0),
+          ),
+        ],
+        //defaultInteractions: true,
         defaultRenderer: charts.ArcRendererConfig(
             arcRendererDecorators: [charts.ArcLabelDecorator()]
         ),
+        /* for interaction with PieChart
+        selectionModels: [
+          charts.SelectionModelConfig(
+            type: charts.SelectionModelType.info,
+            // check for relevant `domain` key in selectedDatum to filter
+            changedListener: (model){
+              try {
+                _selected = model.selectedDatum.first.datum['domain'];
+                return _selected;
+              } catch(e){
+                print("Tapped on outside of pie chart range.");
+              }
+            },
+          ),
+        ],
+         */
       );
-    } else if (data == "list") {
+    } else if (widget.data == "list") {
       return consolidate(context);
     }
   }
